1ef67088bb9fecbd6f981565eeff2e7f41a66143
VishalGowda-bit <vishalygowda@gmail.com>
2024-02-14 16:17:55 +0530

    Organize routes based on this categorization

--- src/Component/Util/Header.jsx
+++ src/Component/Util/Header.jsx
@@ -1,41 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../Images/Flipkart-logo.png";
-
+import logo from "../Images/flipkart logo.png";
 
 const Header = () => {
   return (
     <header className="h-20  flex justify-center ">
-      <nav className="bg-slate-300 flex justify-between items-center h-full w-full p-10">
+      <nav className="bg-white-100 flex justify-stretch items-center h-full w-full p-10">
         <Link>
-          <img src={logo} alt="" className="h-16 w-36 " />
+          <img src={logo} alt="" className="h-10 w-40 " />
         </Link>
 
-        {/* Search Bar */}
-        <div className="w-1/3 flex justify-center items-center bg-blue-50 rounded-md pl-2">
-        <i class="fa-solid fa-magnifying-glass"></i>
+        {/*-------------------------------------------> Search Bar <---------------------------------------------------------*/}
+        <div className="w-1/2 flex justify-center items-center bg-blue-50 rounded-md pl-2">
+          <i class="fa-solid fa-magnifying-glass"></i>
           <input
             type="text"
             placeholder="Seach for products, brands and more...,"
-            className="h-12 w-96  bg-blue-50 rounded-md pl-4  text-lg"
+            className="h-12 w-96  bg-blue-50 rounded-md pl-4  text-lg focus:ring-blue-50 focus:outline-none"
           />
         </div>
 
-        {/* Link Block */}
+        {/*----------------------------------------------------> Link Block <-----------------------------------------------*/}
         <div className="w-96 flex justify-evenly items-center ">
-          {/* Login */}
-          <Link to={"/"}>
-            Login
+          {/*---------------> Login <----------------*/}
+          <div>
+            <Link
+              to={"/login"}
+              className="bg-blue-50 hover:bg-slate-300 active:bg-violet-700 focus:outline-none focus:ring focus:ring-blue-50 p-3 rounded-md "
+            >
+              Login
+            </Link>
+          </div>
+
+          {/* -----------> Become a Seller Option <--------*/}
+          <Link
+            to={"/"}
+            className="hover:bg-blue-50 active:bg-violet-700 focus:outline-none focus:ring focus:ring-blue-50 p-3 rounded-md"
+          >
+            Become a Seller
           </Link>
 
-          {/* Become a Seller Option */}
-          <Link to={"/"}>
-          Become a Seller
-          </Link>
-
-          {/* Cart */}
-          <Link to={"/"}>
-          Cart
+          {/*--------------> Cart <------------ */}
+          <Link
+            to={"/"}
+            className="hover:bg-blue-50 active:bg-violet-700 focus:ring focus:ring-blue-50 p-3 rounded-md hover:outline-offset-2 "
+          >
+            Cart
           </Link>
         </div>
       </nav>
